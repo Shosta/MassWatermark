dc8e606c94380ceaff5d04adaf73a2247bc22792
Remi Lavedrine <remi.lavedrine@outlook.com>
2019-06-17 10:22:32 +0200

    Adding Install script.
    It checks if the Go language is already installed in the computer.
    It the checks if the pdfcpu Go package is already installed. If not, it is intalling it on the computer.
    Then it makes the stamp.sh script available system-wide through /usr/local/bin

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# check for specific command
+# Return values:
+#  0 - command is available
+#  1 - command is not available
+check_for_command(){
+  command -v "$1" 1>/dev/null 2>&-
+}
+
+
+# Check if the Go language is already installed.
+if check_for_command go; then
+    echo "The Go language is installed."
+else
+    echo 'The Go language is not installed.'
+    echo "Please install the Go language first as we are relying on a Go package for that program."
+    echo "Go to https://golang.org/dl/ to install the Go language on your computer."
+    exit 1
+fi
+
+
+# Check if the pdfcpu package is already installed
+if check_for_command $GOBIN/pdfcpu version; then
+    echo "'pdfcpu' is installed"
+else
+    echo "'pdfcpu' is not installed." >&2
+
+    # Install the pdfcpu package.
+    echo "Installing 'pdfcpu'..."
+    go get github.com/hhrutter/pdfcpu/cmd/...
+    go install $GOPATH/src/github.com/hhrutter/pdfcpu/cmd/pdfcpu
+fi
+
+
+# Make the Stamp.sh file available system-wide.
+echo "Installing stamp system-wide (located in /usr/local/bin)..."
+chmod +x ./stamp.sh
+cp ./stamp.sh /usr/local/bin/stamp
+
+echo "'stamp' is available system-wide. Try 'stamp --help' to see how it is working."
